6a5b1f0db4c37ad00a86f5564d2c527231ffc3b6
Philipp Lätsch <xxhelloxx@outlook.de>
2020-02-02 14:44:27 +0100

    Added ps1 script and config example

---
+++ start.ps1
@@ -0,0 +1,42 @@
+﻿$confPath = "$($PSScriptRoot)\\config.json"
+$tmpPath = "$($PSScriptRoot)\\tmp"
+
+if (!(Test-Path $confPath)) {
+    Write-Host -ForegroundColor Red "[ERROR] No config file found!"
+    return
+}
+
+$config = Get-Content $confPath | ConvertFrom-Json
+
+function backupToTargetPath([String] $source, [String] $targetPath) {
+    if (!(Test-Path $source)) {
+        Write-Host -ForegroundColor Red "[ERROR] Source '$($source)' does not exist!"
+        return
+    }
+
+    if (!(Test-Path $targetPath)) {
+        Write-Host -ForegroundColor Red "[ERROR] Target path '$($targetPath)' does not exist!"
+        return
+    }
+
+    Write-Host "Start to copy Backup to '$($targetPath)'"
+    copy $source $targetPath
+    Write-Host -ForegroundColor Green "Copied Backup to '$($targetPath)' !"
+} 
+
+if (!(Test-Path $config.sourcePath)) {
+    Write-Host -ForegroundColor Red "[ERROR] Source '$($config.sourcePath)' does not exist!"
+    return
+}
+
+if ((Test-Path $tmpPath)) {
+    rm "$($tmpPath)\\*"
+} else {
+    mkdir $tmpPath
+}
+
+$zipPath = "$($tmpPath)\\Backup_7dtd-$(get-date -f yyyy-MM-dd--HHmmss).zip"
+Compress-Archive -Path $config.sourcePath -DestinationPath $zipPath
+
+$config.targetPath | ForEach-Object {backupToTargetPath $zipPath $_}
+
