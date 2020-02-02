d305c98653d73e5c174f69db0dc9a676aeb7bd24
Philipp Lätsch <xxhelloxx@outlook.de>
2020-02-02 15:51:20 +0100

    Wait on end if running in console

--- start.ps1
+++ start.ps1
@@ -40,3 +40,10 @@ Compress-Archive -Path $config.sourcePath -DestinationPath $zipPath
 
 $config.targetPath | ForEach-Object {backupToTargetPath $zipPath $_}
 
+# If running in the console, wait for input before closing.
+if ($Host.Name -eq "ConsoleHost")
+{
+    Write-Host "Press any key to continue..."
+    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+}
+
